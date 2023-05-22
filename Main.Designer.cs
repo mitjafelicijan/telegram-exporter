f16e538ccffdcfcc85492f916c9f0ed70eec4abc
PC <PC@DESKTOP-EQCPELL>
2023-05-22 13:51:12 -0700

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace telegram_exporter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCheckForTelegramData = new System.Windows.Forms.Button();
+            this.textBoxAdbPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialogAdbSelector = new System.Windows.Forms.OpenFileDialog();
+            this.buttonSelectAdb = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCheckForTelegramData
+            // 
+            this.buttonCheckForTelegramData.Location = new System.Drawing.Point(426, 172);
+            this.buttonCheckForTelegramData.Name = "buttonCheckForTelegramData";
+            this.buttonCheckForTelegramData.Size = new System.Drawing.Size(217, 43);
+            this.buttonCheckForTelegramData.TabIndex = 0;
+            this.buttonCheckForTelegramData.Text = "Check for Telegram Data";
+            this.buttonCheckForTelegramData.UseVisualStyleBackColor = true;
+            this.buttonCheckForTelegramData.Click += new System.EventHandler(this.buttonCheckForTelegramData_Click);
+            // 
+            // textBoxAdbPath
+            // 
+            this.textBoxAdbPath.Location = new System.Drawing.Point(148, 307);
+            this.textBoxAdbPath.Name = "textBoxAdbPath";
+            this.textBoxAdbPath.Size = new System.Drawing.Size(372, 20);
+            this.textBoxAdbPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 286);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path to ADB.exe";
+            // 
+            // openFileDialogAdbSelector
+            // 
+            this.openFileDialogAdbSelector.FileName = "adb.exe";
+            this.openFileDialogAdbSelector.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialogAdbSelector_FileOk);
+            // 
+            // buttonSelectAdb
+            // 
+            this.buttonSelectAdb.Location = new System.Drawing.Point(527, 303);
+            this.buttonSelectAdb.Name = "buttonSelectAdb";
+            this.buttonSelectAdb.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectAdb.TabIndex = 3;
+            this.buttonSelectAdb.Text = "Browse";
+            this.buttonSelectAdb.UseVisualStyleBackColor = true;
+            this.buttonSelectAdb.Click += new System.EventHandler(this.buttonSelectAdb_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSelectAdb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAdbPath);
+            this.Controls.Add(this.buttonCheckForTelegramData);
+            this.Name = "Main";
+            this.Text = "Telegram Data Exporter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCheckForTelegramData;
+        private System.Windows.Forms.TextBox textBoxAdbPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialogAdbSelector;
+        private System.Windows.Forms.Button buttonSelectAdb;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace telegram_exporter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            // Re-populate adb text field if it was already set in the past.
+            string adbPathSetting = Properties.Settings.Default.AdbPath;
+            textBoxAdbPath.Text = adbPathSetting;
+
+            string command = "devices";
+            string compoundCommand = String.Format("/c {0} {1}", adbPathSetting, command);
+
+            Console.WriteLine(compoundCommand);
+        }
+
+        private string[] executeShellAndReturn(string command)
+        {
+            string adbPathSetting = Properties.Settings.Default.AdbPath;
+            string compoundCommand = String.Format("/c {0} {1}", adbPathSetting, command);
+
+            var processStartInfo = new ProcessStartInfo("cmd", compoundCommand)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = new Process
+            {
+                StartInfo = processStartInfo
+            };
+
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string[] response = new string[] { };
+            string[] lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+            foreach (var line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    Array.Resize(ref response, response.Length + 1);
+                    response[response.Length - 1] = line;
+                }
+            }
+
+            Console.WriteLine(response);
+
+            return response;
+        }
+
+        private void buttonCheckForTelegramData_Click(object sender, EventArgs e)
+        {
+            string[] output = executeShellAndReturn("shell ls /sdcard/Telegram1");
+
+            if (output.Length == 0)
+            {
+                Console.WriteLine("No Telegram Data detected!");
+            }
+
+            foreach (var line in output)
+            {
+                if (!line.StartsWith("List of"))
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        private void buttonSelectAdb_Click(object sender, EventArgs e)
+        {
+            openFileDialogAdbSelector.ShowDialog();
+        }
+
+        private void openFileDialogAdbSelector_FileOk(object sender, CancelEventArgs e)
+        {
+            textBoxAdbPath.Text = openFileDialogAdbSelector.FileName;
+
+            // Save for later uses.
+            Properties.Settings.Default.AdbPath = openFileDialogAdbSelector.FileName;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
